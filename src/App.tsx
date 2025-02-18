651ec8191cb90cc990ce093eee976feb504ba540
Swearfy <swearfy@gmail.com>
2025-02-18 21:05:49 +0200

    init comit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>ROONA PORTOFOOLIO</h1>
+      </div>
+
+      <img src="./src/assets/roona.jpg" alt="" />
+    </>
+  );
+}
+
+export default App;
